cf4424da95e6107a38b8088ebc7f06a715afb70a
Antonin Nhek <a.nhek@icloud.com>
2019-11-26 13:17:25 +0100

    added: offline mode + pwa settings.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,14 @@
 
 require('typeface-fira-sans');
 require('typeface-work-sans');
+
+export const onServiceWorkerUpdateReady = () => {
+  const answer = window.confirm(
+    `This application has been updated. ` +
+      `Reload to display the latest version?`
+  )
+
+  if (answer === true) {
+    window.location.reload()
+  }
+}
--- gatsby-config.js
+++ gatsby-config.js
@@ -39,13 +39,19 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/anhek-icon.png`, // This path is relative to the root of the site.
+        name: "Antonin Nhek | Visual Designer & Front-End Developer",
+        short_name: "Antonin Nhek Portfolio",
+        start_url: "/",
+        background_color: "#1a202c",
+        theme_color: "#c7ff84",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/anhek-icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
       },
     },
     {
@@ -71,6 +77,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
