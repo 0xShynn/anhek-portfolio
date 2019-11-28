b259849f8ac6d672e9bdf91fc125560079eddff2
Antonin Nhek <a.nhek@icloud.com>
2019-11-28 15:16:11 +0100

    updated: about page en/fr. cleaned main.css

--- src/components/about.js
+++ src/components/about.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 import Portrait from './portrait';
 import Obfuscate from "react-obfuscate"
-import { GoMail } from "react-icons/go"
+import { FiSend } from "react-icons/fi"
 
 
 const About = () => {
@@ -13,23 +13,27 @@ const About = () => {
       </div>    
       <div className="text-gray-800 dark:text-white">
         <FormattedMessage id="about.title">
-          {txt => <p className="text-2xl md:text-3xl text-gray-600 dark:text-secondary_light">{txt}</p>}
+          {txt => <p className="text-2xl mb-6 md:text-3xl text-gray-600 dark:text-secondary_light">{txt}</p>}
         </FormattedMessage>
         <FormattedMessage id="about.intro">
-          {txt => <p className="py-4 sm:py-8 md:py-10 text-xl font-semibold leading-relaxed">{txt}</p>}
-        </FormattedMessage>
-        <p className="mb-10 md:mb-16">
+          {txt => <p className="text-xl font-semibold leading-relaxed">{txt}</p>}
+        </FormattedMessage><br />
+        <p className="mb-6 md:mb-10">
           <FormattedMessage id="about.text1" /><br /><br />
           <FormattedMessage id="about.text2" /><br /><br />
           {/* <FormattedMessage id="about.text3" /><br /><br /> */}
           <FormattedMessage id="about.text4" />
         </p>
-        <p className="text-lg md:text-2xl mb-8 md:mb-16 p-4 md:p-8 bg-gray-200 dark:bg-gray-800 dark:text-white rounded-lg">
+        <p className="text-lg md:text-xl font-semibold mb-8 md:mb-12 dark:text-secondary_light">
           <FormattedMessage id="about.text5" />
-        </p>
-        <div className="text-center">
-          <p><FormattedMessage id="about.dropamail" /></p>
-          <Obfuscate email="hello@anhek.dev" className="text-center text-2xl text-primary dark:text-secondary"/>
+        </p> 
+        <div className="flex justify-center">
+          <div className="text-center py-3 px-6 md:py-4 md:px-8 rounded dark:bg-gray-800 bg-gray-200 inline-block">
+            <p className="text-gray-700 dark:text-gray-300 text-base md:text-xl">
+              <FormattedMessage id="about.dropamail" />
+            </p>
+            <Obfuscate email="hello@anhek.dev" className="text-center text-2xl md:text-3xl text-primary dark:text-secondary dark:hover:text-secondary_light"/>
+          </div>
         </div>
       </div>
     </section>
