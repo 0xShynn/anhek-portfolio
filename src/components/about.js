923e8d543d24d23549cf341480903fdedb8c0483
Antonin Nhek <a.nhek@icloud.com>
2019-11-28 18:41:04 +0100

    updated: about page content.

--- src/components/about.js
+++ src/components/about.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import Portrait from './portrait';
 import Obfuscate from "react-obfuscate"
 import { FiSend } from "react-icons/fi"
+import { MdKeyboardArrowRight } from "react-icons/md"
 
 
 const About = () => {
@@ -12,23 +13,78 @@ const About = () => {
         <Portrait/>
       </div>    
       <div className="text-gray-800 dark:text-white">
-        <FormattedMessage id="about.title">
-          {txt => <p className="text-2xl mb-6 md:text-3xl text-gray-600 dark:text-secondary_light">{txt}</p>}
-        </FormattedMessage>
-        <FormattedMessage id="about.intro">
-          {txt => <p className="text-xl font-semibold leading-relaxed">{txt}</p>}
-        </FormattedMessage><br />
-        <p className="mb-6 md:mb-10">
-          <FormattedMessage id="about.text1" /><br /><br />
-          <FormattedMessage id="about.text2" /><br /><br />
-          {/* <FormattedMessage id="about.text3" /><br /><br /> */}
+        <p className="text-2xl mb-6 md:text-3xl text-gray-600 dark:text-secondary_light">
+          <FormattedMessage id="about.title" />
+        </p>
+        <p className="text-xl md:text-2xl font-header font-bold leading-snug">
+          <FormattedMessage id="about.intro" /><br /><br /><br />
+        </p>
+        <p className="">
+          <FormattedMessage id="about.text1" /><br /><br /><br />
+          <FormattedMessage id="about.text2" /><br /><br /><br />
+        </p>
+        <div className="flex flex-col sm:flex-row">
+          <ul className="text-lg mr-10 list">
+            <li className="text-xl font-bold uppercase font-header text-gray-700 dark:text-secondary_light mb-4">
+              <FormattedMessage id="about.interests.title" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.interests.1" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.interests.2" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.interests.3" />            
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.interests.4" />
+            </li>
+            <li className="mb-6 sm:mb-0">
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.interests.5" />
+            </li>
+          </ul>
+          <ul className="text-lg list">
+            <li className="text-xl font-bold uppercase font-header text-gray-700 dark:text-secondary_light mb-4">
+              <FormattedMessage id="about.personality.title" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.personality.1" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.personality.2" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.personality.3" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.personality.4" />
+            </li>
+            <li>
+              <MdKeyboardArrowRight/>
+              <FormattedMessage id="about.personality.5" />
+            </li>
+          </ul>  
+        </div>
+        <br /><br /><br />
+        <p className="pb-10 md:pb-16">
+          <FormattedMessage id="about.text3" />
+        </p>
+        <p className="text-xl md:text-2xl dark:text-secondary_light border-t border-gray-400 dark:border-gray-700 pt-10 md:pt-16">
           <FormattedMessage id="about.text4" />
         </p>
-        <p className="text-lg md:text-xl font-semibold mb-8 md:mb-12 dark:text-secondary_light">
-          <FormattedMessage id="about.text5" />
-        </p> 
+        <br /><br />
         <div className="flex justify-center">
-          <div className="text-center py-3 px-6 md:py-4 md:px-8 rounded dark:bg-gray-800 bg-gray-200 inline-block">
+          <div className="text-center py-3 px-6 md:py-4 md:px-8 rounded border border-gray-400 dark:border-gray-700 inline-block">
             <p className="text-gray-700 dark:text-gray-300 text-base md:text-xl">
               <FormattedMessage id="about.dropamail" />
             </p>
