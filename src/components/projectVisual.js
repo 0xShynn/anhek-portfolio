33df110a76d087635c0a95ed73f5eeb01db1f4c1
Antonin Nhek <a.nhek@icloud.com>
2019-12-02 22:54:05 +0100

    update: padding projects on graphic design page.

--- src/components/projectVisual.js
+++ src/components/projectVisual.js
@@ -5,7 +5,7 @@ import { injectIntl } from "gatsby-plugin-intl"
 const ProjectVisual = ({ title, img}) => {
   
   return (
-    <div className="w-full px-6 mb-12 md:px-3 lg:px-6 sm:mb-16">
+    <div className="w-full px-3 mb-10 md:px-3 lg:px-6 sm:mb-12 md:mb-16 lg:mb-20">
       <Img
         fluid={img}
         className="rounded shadow-md"
