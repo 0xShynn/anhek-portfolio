45626fbb1667d6e417b8ea59598a3318f55b7044
Antonin Nhek <a.nhek@icloud.com>
2019-12-23 20:54:34 +0100

    added: résumé download link.

--- src/components/about.js
+++ src/components/about.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
+import { useIntl } from "gatsby-plugin-intl"
 import Portrait from './portrait';
 import Obfuscate from "react-obfuscate"
-import { MdKeyboardArrowRight } from "react-icons/md"
+import { MdKeyboardArrowRight, MdFileDownload } from "react-icons/md"
+import { GoMail } from "react-icons/go"
 
 
 const About = () => {
+  const intl = useIntl();
+
   return (
     <section id="about" className="flex flex-col max-w-md px-6 py-12 mx-auto sm:px-10 md:px-6 sm:max-w-2xl md:max-w-2xl lg:max-w-3xl sm:py-20 md:py-24 lg:py-32">
       <div className="w-48 mx-auto mb-6 overflow-hidden rounded-full md:w-56 md:mb-8">
@@ -78,11 +82,18 @@ const About = () => {
         <p className="pb-10 md:pb-16">
           <FormattedMessage id="about.text3" />
         </p>
-        <p className="pt-10 text-xl font-semibold text-gray-700 border-t border-gray-400 md:text-2xl dark:text-secondary_light dark:border-gray-700 md:pt-16">
+        <p className="pt-10 text-xl font-semibold text-center text-gray-700 border-t border-gray-400 md:text-2xl dark:text-secondary_light dark:border-gray-700 md:pt-16">
           <FormattedMessage id="about.text4" />
         </p>
-        <div className="flex justify-center mt-6 md:mt-10">
-          <Obfuscate email="hello@anhek.dev" className="px-5 py-3 text-xl border rounded-lg border-primary dark:border-secondary md:py-4 md:px-8 md:text-2xl text-primary hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-secondary"/>
+        <div className="flex flex-wrap justify-center mt-6 text-xl md:mt-10">
+          <a className="px-5 py-3 mb-4 mr-0 border rounded-lg sm:mb-0 sm:mr-4 border-primary dark:border-secondary md:py-4 md:px-8 md:text-2xl text-primary hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-secondary" href={intl.formatMessage({ id: "about.resume" })}>
+            <MdFileDownload className="inline"/> <FormattedMessage id="about.downloadResume" />
+          </a>
+          <Obfuscate email="hello@anhek.dev">
+            <button className="px-5 py-3 border rounded-lg border-primary dark:border-secondary md:py-4 md:px-8 md:text-2xl text-primary hover:bg-gray-200 dark:hover:bg-gray-800 dark:text-secondary">
+              <GoMail className="inline mb-1 mr-1"/> <FormattedMessage id="about.sendMail" />
+            </button>
+          </Obfuscate>
         </div>
       </div>
     </section>
